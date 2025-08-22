0e544d020d5bf2115bab3a77adaa8942bb4c40b9
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2025-08-22 00:14:48 -0300

    Adaptação dos valores

--- plotPolinominalGraph.py
+++ plotPolinominalGraph.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt  # Usado para criar gráficos e visualizações
 import numpy as np  # Numpy é uma biblioteca fundamental para operações matemáticas e manipulação de arrays
 
 # Entrada da função
-entradas = np.array([1, 2, 3, 4, 5, 6])
+entradas = np.array([2, 6 ,7 ,9, 14, 20, 18])
 
 #Saidas da função
-saidas = np.array([1, 4, 6, 8, 10, 12])
+saidas = np.array([5 ,7 ,10 , 9, -4, 22, 20])
 
 # polyfit é um objeto usada para descobrir a função polinominal que corresponde as entradas e saidas apresentadas. Estrutura: (valores de entrada, valores de saida, numero de polinômios)
-coeficientes = np.polyfit(entradas, saidas, 2)
+coeficientes = np.polyfit(entradas, saidas, 6)
 
 # o objeto poly1d tem a capacidade de aplicar baseado nos coeficientes que vão dentro dos (). Logo, atribuir essa função a uma variável a torna capaz de devolver o valor de y para qualquer x
 modelo = np.poly1d(coeficientes)
@@ -29,11 +29,11 @@ def plotGraph (entradas_x, saidas_y):
     # Utilizaremos o plt.plot novamente, dessa vez usando a marcação "r--" para criar uma linha de tendência
     plt.plot(x_fit, y_fit, 'r--', label=f'Modelo: {modelo}')
     # Titulo do gráfico
-    plt.title('Migracao por ano')
+    plt.title('Altos e Baixos')
     # Legenda X
-    plt.xlabel('Ano')
+    plt.xlabel('Números de Entrada')
     # Legenda Y
-    plt.ylabel('Migracao')
+    plt.ylabel('Números de Saída')
     # Ativa a legenda
     plt.legend()
     # Ativa o fundo grid
